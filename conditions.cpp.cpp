caba76b5cbed63b3558ee0b99b84f6c826821191
kendyivy <35097006+kendyivy@users.noreply.github.com>
2018-03-01 12:55:54 +0300

    Add files via upload

---
+++ conditions.cpp.cpp
@@ -0,0 +1,36 @@
+https://www.onlinegdb.com/online_c++_compiler#editor_1/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Ask for 1st number";
+    
+      cin>> a;
+      cout<<" Ask for 2nd number";
+      cin>>b;
+      if(a>b){
+          cout<< "a is greater";
+      }
+      else if(a==b){
+          cout<<"a is equal to be b";
+      }
+      else{"b is greater";}
+      
+      
+
+    return 0;
+}
+
+
+
