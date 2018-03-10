ccba162433f0dc7ec129f4b79b4cdf676c6ccccc
kendyivy <35097006+kendyivy@users.noreply.github.com>
2018-03-10 11:49:00 +0300

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+var student = ["Jane","Mary","Lauren","Ivy","Fatma"]
+var shoesSize=[31,42,33,24,38]
+//Get the lenth of the Array
+console.log(student.length)
+
+for(i=0;i<student.length;i+=1){
+	console.log(student[i]);
+}
