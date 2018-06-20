445292e6903b9c13ee944a758915e159490f7d6a
kendyivy <35097006+kendyivy@users.noreply.github.com>
2018-06-20 09:17:46 +0300

    Add files via upload

---
+++ FatArrow.js
@@ -0,0 +1,9 @@
+var add = (a,b,c,d) => a + b + c + d;
+console.log(add(3,5,7,30))
+
+var subtract = (a,b) => a - b;
+console.log(subtract(30,8))
+
+var multiply = (a,b,c,) => a * b * c;
+console.log(multiply(3,5,7))
+
